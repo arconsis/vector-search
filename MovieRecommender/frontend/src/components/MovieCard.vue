5192d93e0341cd7671f7d13dffa25044ac607738
MaxManthey <maximilian.manthey@arconsis.com>
2022-12-19 11:20:54 +0100

    changed movie cards and carousel layout

--- MovieRecommender/frontend/src/components/MovieCard.vue
+++ MovieRecommender/frontend/src/components/MovieCard.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="card">
+  <div class="card" @mouseover="showContent = true" @mouseout="showContent = false">
     <div class="card-image">
       <figure class="image is-3by4">
         <img :src="movie.imageUrl" alt="Placeholder image" />
       </figure>
     </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-content">
+    <header class="card-header">
+      <div class="title-wrapper">
+        <span>
           <p class="title is-4">{{ movie.name }}</p>
           <p class="subtitle is-6">Certainty: {{ movie.certainty }}</p>
-        </div>
+        </span>
+        <span class="icon-wrapper">
+          <b-icon
+            v-if="showContent"
+            icon="chevron-down"
+            size="is-medium"
+          ></b-icon>
+          <b-icon
+            v-else
+            icon="chevron-up"
+            size="is-medium"
+          ></b-icon>
+        </span>
       </div>
-
-      <div class="content">{{ movie.description }}</div>
-    </div>
+    </header>
+    <transition name="dropdown">
+      <div class="card-content" v-if="showContent">
+        <div class="content">{{ movie.description }}</div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -25,7 +40,29 @@ export default Vue.extend({
   props: {
     movie: Object,
   },
+  data() {
+    return {
+      showContent: false,
+    }
+  },
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.title-wrapper {
+  margin: .6rem auto .1rem auto;
+}
+
+.dropdown-enter-active {
+  transition: all 1s;
+}
+
+.dropdown-leave-active {
+  transition: all .5s;
+}
+
+.dropdown-enter, .dropdown-leave-to {
+  transform: translateY(-1.5rem);
+  opacity: 0;
+}
+</style>
